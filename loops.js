32928acc9b9dbca36ddaa6d621d65a5669dd163a
sarah dagamseh <dagamsehsarah@gmail.com>
2021-03-08 13:18:34 +0200

    add function

--- loops.js
+++ loops.js
@@ -1,9 +1,14 @@
 
 var order = prompt(" what is your favorite dish chicken or beef ?");
-while (order !='chicken' && order !='beef')
+var menu = function()
 {
+ while (order !='chicken' && order !='beef')
+ {
+    
     order=prompt(" what is your favorite dish chicken or beef ?");
+ }
 }
+ menu ();
 var itemOrder = '';
 
 if(order == 'chicken')
@@ -15,13 +20,15 @@ else
     itemOrder ='<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQpSk1vwAYtpeeUGMCYSelTIXcyFqsFs5_9Qw&usqp=CAU"/>';
 }
 
-var numOfMeals = prompt("how many meals do you want?");
+ var numOfMeals = prompt("how many meals do you want?");
+ function result ()
+ { var result = '';
 
-var result = '';
-
-for (var i = 0; i <numOfMeals; i++)
-{
+ for (var i = 0; i <numOfMeals; i++)
+ {
     result = result + itemOrder;
-}
+ }
 
-document.write(result);
+ document.write(result);
+ }
+ result();
